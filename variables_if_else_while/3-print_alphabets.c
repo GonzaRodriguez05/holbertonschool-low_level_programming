212c54990c774f92bb23df3fde5de5eadbaef561
GonzaRodriguez05 <8668@holbertonstudents.com>
2024-02-16 13:39:44 +0000

    hola

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Pint alphabet
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+        char a;
+
+        for (a = 'a'; a <= 'z'; a++)
+        {
+                putchar(a);
+        }
+
+	for (a ='A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+        putchar('\n');
+
+
+
+return (0);
+
+}
