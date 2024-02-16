1af33afb84352062924b8a796f1daf19fd492f09
GonzaRodriguez05 <8668@holbertonstudents.com>
2024-02-16 14:32:29 +0000

    hola

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Pint alphabet
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char z;
+
+	for (z = 'z'; z >= 'a'; z--)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+
+
+
+return (0);
+
+}
