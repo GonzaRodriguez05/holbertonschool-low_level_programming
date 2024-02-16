83b3c8f9bf94d4eab1cddd79619cd8a1af93270e
GonzaRodriguez05 <8668@holbertonstudents.com>
2024-02-16 13:28:21 +0000

    hola

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) 
+{
+	char a; 
+
+	for(a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		putchar('\n');
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return (0);
+
+}
