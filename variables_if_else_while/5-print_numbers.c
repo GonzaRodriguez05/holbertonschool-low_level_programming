b928276ab1359d5bb02d768f5e2c7e73c0e5cd91
GonzaRodriguez05 <8668@holbertonstudents.com>
2024-02-16 14:10:35 +0000

    hola

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Pint number base 10
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+
+int num;
+
+for (num = 0; num < 10; num++)
+{
+	printf("%d", num);
+}
+
+return (0);
+}
+
