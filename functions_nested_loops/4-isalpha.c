e04f6a2d0176de633f0b7e426aedf4432cea36a7
GonzaRodriguez05 <8668@holbertonstudents.com>
2024-02-19 14:19:34 +0000

    hola

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+if (c >= 'A' && c <= 'Z')
+
+return (1);
+return (0);
+}
