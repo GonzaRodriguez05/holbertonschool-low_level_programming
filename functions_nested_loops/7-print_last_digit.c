9a4519829e65fecbf7245fd8adef4d6be4d17a8a
GonzaRodriguez05 <8668@holbertonstudents.com>
2024-02-20 13:24:55 +0000

    hola

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - check the code for Holberton School students.
+ * 
+ *
+ * Return: return
+ */
+int print_last_digit(int n)
+{
+
+	int ul = n % 10;
+
+	if (ul < 0)
+	ul = -ul;
+	 _putchar(ul + '0');
+	return (-ul);
+	
+}
