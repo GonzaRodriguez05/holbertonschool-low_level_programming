f5ea4e1c0b37a0422645dbcec953168f2709a057
GonzaRodriguez05 <8668@holbertonstudents.com>
2024-02-20 12:54:43 +0000

    hola

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,11 +1,19 @@
 #include "main.h"
+/**
+ * _abs - check the code for Holberton School students.
+ * @n : number
+ *
+ * Return: return
+ */
+
 int _abs(int n)
 {
 if (n >= 0)
+
 	return (n);
 
 return (-n);
- 
+
 
 
 }
